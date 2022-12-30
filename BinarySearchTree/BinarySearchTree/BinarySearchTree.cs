40306211bd410c34e23199660e165885692489dc
qismat <qismatnisha6243@gmail.com>
2022-12-30 21:13:17 +0530

    [qismat] Add : UC1-CreateBTSElement

---
+++ BinarySearchTree/BinarySearchTree/BinarySearchTree.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class BinarySearchTree<T> where T : IComparable
+    {
+        //UC1
+        //create a BST
+        int count = 0;
+        bool result = false;
+        Node<T> Root;
+        Node<T> Current;
+        public void InsertData(T data)
+        {
+            if (Root == null)
+            {
+                this.Root = new Node<T>(data);
+                this.Current = Root;
+                count++;
+                return;
+            }
+            if (this.Current.data.CompareTo(data) > 0)
+            {
+                if (this.Current.leftNode == null)
+                {
+                    this.Current.leftNode = new Node<T>(data);
+                    this.Current = Root;
+                    count++;
+                }
+                else
+                {
+                    this.Current = this.Current.leftNode;
+                    InsertData(data);
+                }
+            }
+            else
+            {
+                if (this.Current.rightNode == null)
+                {
+                    this.Current.rightNode = new Node<T>(data);
+                    this.Current = Root;
+                    count++;
+                }
+                else
+                {
+                    this.Current = this.Current.rightNode;
+                    InsertData(data);
+                }
+            }
+        }
+        public Node<T> GetRoot()
+        {
+            return this.Root;
+        }
+        //UC2
+        //Count Of Element in Binary Search Tree
+        public int GetSize()
+        {
+            return count;
+        }
+        //UC3
+        //Search elements in BST
+        public bool SearchTree(int data, Node<T> node)
+        {
+            if (node == null)
+            {
+                return false;
+            }
+            else
+            {
+                if (this.Current.data.Equals(data))
+                {
+                    result = true;
+                }
+                else if (this.Current.data.CompareTo(data) > 0)
+                {
+                    this.Current = this.Current.leftNode;
+                    SearchTree(data, Current);
+                }
+                return result;
+            }
+        }
+        public void Display(Node<T> node)
+        {
+            if (node != null)
+            {
+                Display(node.leftNode);
+                Console.WriteLine("Element in binary serach tree is: " + node.data);
+                Display(node.rightNode);
+            }
+        }
+    }
+}
---
+++ BinarySearchTree/BinarySearchTree/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace BinarySearchTree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BinarySearchTree<int> binarySearchTree = new BinarySearchTree<int>();
+            binarySearchTree.InsertData(56);
+            binarySearchTree.InsertData(30);
+            binarySearchTree.InsertData(70);
+            binarySearchTree.Display(binarySearchTree.GetRoot());
+            Console.WriteLine("Count Of Element in Binary Search Tree is : " + binarySearchTree.GetSize());
+            bool result = binarySearchTree.SearchTree(30, binarySearchTree.GetRoot());
+            Console.WriteLine(result);
+        }
+    }
+}
