d3e7b4278b22bd7114f17e352b2ccb4f083539f9
qismat <qismatnisha6243@gmail.com>
2022-12-30 21:21:24 +0530

    [qismat] Add : UC1-CreateBTSElement

--- BinarySearchTree/BinarySearchTree/Program.cs
+++ BinarySearchTree/BinarySearchTree/Program.cs
@@ -9,9 +9,6 @@
             binarySearchTree.InsertData(30);
             binarySearchTree.InsertData(70);
             binarySearchTree.Display(binarySearchTree.GetRoot());
-            Console.WriteLine("Count Of Element in Binary Search Tree is : " + binarySearchTree.GetSize());
-            bool result = binarySearchTree.SearchTree(30, binarySearchTree.GetRoot());
-            Console.WriteLine(result);
         }
     }
 }
